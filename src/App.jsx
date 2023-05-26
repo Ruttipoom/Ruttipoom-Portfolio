9bdcee7a61f4aee8b2df86871b7e91fd61a5c8b8
Ruttipoom <Ruttipoomrtp8@gmail.com>
2023-05-26 18:08:54 +0700

    recheck

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Header from './components/header/Header'
 import Home from '../src/components/home/Home'
 import About from '../src/components/about/About'
 import Skills from './components/skill/Skill'
-import Services from './components/services/Services'
+/* import Services from './components/services/Services' */
 import Qualification from './components/qualification/Qualification'
-import Testimonials from './components/testimonials/Testimonials'
+/* import Testimonials from './components/testimonials/Testimonials' */
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Scrollup from './components/contact/scrollup/Scrollup'
