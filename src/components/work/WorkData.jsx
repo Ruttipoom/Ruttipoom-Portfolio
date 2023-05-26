84ec85b0b94589f1eb037b3e0184ba34497b301f
Ruttipoom <Ruttipoomrtp8@gmail.com>
2023-05-26 18:14:19 +0700

    recorrect

--- src/components/work/WorkData.jsx
+++ src/components/work/WorkData.jsx
@@ -1,6 +1,6 @@
-import Work1 from "../../assets/work1.jpg"
-import Work2 from "../../assets/work2.jpg"
-import Work3 from "../../assets/work3.jpg"
+import Work1 from "../../assets/Work1.jpg"
+import Work2 from "../../assets/Work2.jpg"
+import Work3 from "../../assets/Work3.jpg"
 
 
 export const projectsData = [
